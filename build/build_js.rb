edf92f85faa0f57ae81b7b1e360b2fc46d54029e
jamesformica <james.formica@gmail.com>
2015-08-15 14:05:40 +1000

    typescript compiles on app start

--- build/build_js.rb
+++ build/build_js.rb
@@ -1,34 +1,40 @@
 require 'typescript-node'
 require 'colorize'
 
-puts "\n### Building Typescript files ###\n".magenta
+class TypescriptBuilder
+
+  def self.build
+
+    puts "\n### Building Typescript files ###\n".magenta
 
 # make sure node exists
-TypeScript::Node.check_node
+    TypeScript::Node.check_node
 
 # clean out the js folder
-FileUtils.rm_rf(Dir.glob('./app/js/*'))
+    FileUtils.rm_rf(Dir.glob('./app/js/*'))
 
 # generate the js for each ts file
 
-error = false
-Dir.glob('../app/ts/*.ts') do |tsFile|
-  result = TypeScript::Node.compile_file(tsFile, '--target', 'ES5')
-  if result.success?
-    js_file_name = tsFile.gsub!("ts", "js")
-    out_file = File.new(js_file_name, "w")
-    out_file.puts(result.js)
-    out_file.close
-    puts "- #{tsFile} \t" + "done".green
-  else
-    puts result.stdout.red
-    error = true
+    error = false
+    Dir.glob(File.dirname(__FILE__) + '/../app/ts/*.ts') do |tsFile|
+      result = TypeScript::Node.compile_file(tsFile, '--target', 'ES5')
+      if result.success?
+        js_file_name = tsFile.gsub!("ts", "js")
+        out_file = File.new(js_file_name, "w")
+        out_file.puts(result.js)
+        out_file.close
+        puts "- #{tsFile} \t" + "done".green
+      else
+        puts result.stdout.red
+        error = true
+      end
+    end
+
+    puts "\nTypescript compilation complete.".green
+
+    if !error
+      sleep(2)
+      system 'clear'
+    end
   end
-end
-
-puts "\nTypescript compilation complete.".green
-
-if !error
-  sleep(2)
-  system 'clear'
 end
--- init.rb
+++ init.rb
@@ -1,3 +1,7 @@
 require_relative "helpers/container_helper"
+require_relative "build/build_js"
 
 Trains.helpers ContainerHelpers
+
+# compile the typescript on app startup
+TypescriptBuilder.build
